6ae7a4d370a64ffd3daf141ab7992ab92d083197
Joel <joellefkowitz@hotmail.com>
2021-03-05 13:45:56 +0000

    Test travis coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,15 @@ function quit(name) {
 module.exports = function (grunt) {
   grunt.initConfig({
     exec: {
-      lint: "pylint",
+      lint: "echo \"Lint not implemented\"",
       testsUnit: "tox",
-      testsCoverage: "pytest --cov=quickdocs tests/",
-      docsBuild: "sphinx-build docs build"
-    }
+      testsCoverage: "pytest --cov=quickdocs tests/ --cov-report=xml:cov.xml",
+      docsBuild: "sphinx-build docs build",
+    },
   });
-  grunt.loadNpmTasks('grunt-exec')
+  grunt.loadNpmTasks("grunt-exec");
   grunt.registerTask("lint", "exec:lint");
-  grunt.registerTask("tests:unit", "exec:testUnit");
-  grunt.registerTask("tests:coverage", "exec:testCoverage");
+  grunt.registerTask("tests:unit", "exec:testsUnit");
+  grunt.registerTask("tests:coverage", "exec:testsCoverage");
   grunt.registerTask("docs:build", "exec:docsBuild");
 };
