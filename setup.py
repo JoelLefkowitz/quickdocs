376ed1aeb191361fb2a6cf8cac234c6e216d60fc
Joel <joellefkowitz@hotmail.com>
2021-03-05 13:29:00 +0000

    Travis generate coverage for one env only

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
                 "pytest-sugar",
                 "pytest-watch",
                 "pytest",
+                "tox-travis",
                 "tox",
             ],
             "tools": [
