95ee41d4858fc8b841cf86f2ba4b93635d5e96f6
Joel <joellefkowitz@hotmail.com>
2021-03-13 19:23:05 +0000

    WIP Add state tests

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,6 +1,18 @@
-def test_paths():
-    pass
+from quickdocs.state.inputs import Inputs
+from quickdocs.state.paths import Paths
 
 
-def test_inputs():
-    pass
+def test_paths() -> None:
+    """
+    Test Path state class.
+    """
+    paths = Paths(template_path="", template_dir="", output_dir="")
+    assert paths.dct != {}
+
+
+def test_inputs() -> None:
+    """
+    Test Inputs state class.
+    """
+    inputs = Inputs(project="", version="", author="", html_title="", github_url="")
+    assert inputs.dct != {}
